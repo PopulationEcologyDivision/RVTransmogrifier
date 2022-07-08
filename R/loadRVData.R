9a21730269aff454e4cbe93b0809b30267b2cdd2
McMahon <Mike.McMahon@dfo-mpo.gc.ca>
2022-07-08 10:48:30 +0000

    adds loadRVData function

--- R/loadRVData.R
+++ R/loadRVData.R
@@ -1,3 +1,9 @@
+#' @title loadRVData
+#' @description This function loads all of the tables from the RVSurveyData package into a single 
+#' list.
+#' @returns a list of the ~18 data frames from the RVSurveyData package
+#' @author Mike McMahon
+#' @export
 loadRVData<- function(){
   tbls <- RVSurveyData::listTbls()
   dataEnv<-new.env()
